ba49625d6b40861784c57eebe5b4a5dda048f904
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-01-07 22:31:32 -0600

    add files

---
+++ src/home.php
@@ -0,0 +1,21 @@
+<?php include('functions.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('header.php'); ?>
+<title>Tasks</title>
+</head>
+<body>
+
+    <?php include('navbar.php'); ?>
+
+    <div class="container">
+        <h1 class="text-center mt-5">Tasks - home</h1>
+
+    </div>
+
+<?php include('footer.php'); ?>
+<script src="js/home-js.js"></script>
+
+</body>
+</html>
