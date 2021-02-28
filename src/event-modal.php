da2679b564f321d081fa86621a5cc202ee4f2169
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-02-28 11:45:01 -0600

    re-arrange the normal display

--- src/event-modal.php
+++ src/event-modal.php
@@ -2,28 +2,42 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <div>
+                <div class="d-flex justify-content-between align-items-start">
+                    <!-- title -->
                     <h4 class="modal-title"></h4>
-                    <p class="description"></p>
 
-                    <div class="event-data event-data-normal occurs-on">
-                        <div class="event-data-icon"><i class='bx bx-time'></i></div>
-                        <div class="event-data-data"></div>
-                    </div>
-
-                    <div class="event-data event-data-normal address">
-                        <div class="event-data-icon"><i class='bx bx-been-here'></i></div>
-                        <div class="event-data-data"></div>
-                    </div>
-
-                    <div class="event-data event-data-normal phone">
-                        <div class="event-data-icon"><i class='bx bx-phone'></i></div>
-                        <div class="event-data-data"></div>
-                    </div>
+                    <!-- button to close the modal -->
+                    <button type="button" class="close" data-dismiss="modal">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <button type="button" class="close" data-dismiss="modal">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+
+                <!-- description -->
+                <p class="description"></p>
+
+                <!-- occurs on -->
+                <div class="event-data event-data-normal occurs-on">
+                    <div class="event-data-icon"><i class='bx bx-time'></i></div>
+                    <div class="event-data-data"></div>
+                </div>
+
+                <!-- address -->
+                <div class="event-data event-data-normal address">
+                    <div class="event-data-icon"><i class='bx bx-been-here'></i></div>
+                    <div class="event-data-data"></div>
+                </div>
+
+                <!-- phone number -->
+                <div class="event-data event-data-normal phone">
+                    <div class="event-data-icon"><i class='bx bx-phone'></i></div>
+                    <div class="event-data-data"></div>
+                </div>
+
+                <!-- event action buttons -->
+                <div class="d-flex align-items-center justify-content-start">
+                    <button type="button" class="btn btn-sm btn-light" title="Edit"><i class='bx bxs-pencil'></i></button>
+                    <button type="button" class="btn btn-sm btn-light" title="Delete event"><i class='bx bx-trash'></i></button>
+                </div>
             </div>
 
             <div class="modal-body">
